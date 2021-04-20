4df3544e935d392a03d1800100a5c9e7d988382c
Shravan Vallaban <kuppu.shravan@gmail.com>
2021-04-19 19:27:00 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
+from mongoengine import connect
+from flask_mongoengine import MongoEngine
+import pymongo
+import pprint
+app = Flask(__name__)
+
+app.config["MONGODB_DB"] = "myFirstDatabase"
+app.config['MONGO_URI'] = 'mongodb+srv://admin:admin@cluster0.og2k6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
+@app.route('/getEliteUsers', methods=['GET'])
+def get_elite_users():
+  data = mongo.db.myData.aggregate([{"$limit": 20}, {"$match": {"elite": {"$ne": '"'}}}])
+  output = []
+  for obj in data:
+    # print("this is the output", obj['name'])
+    output.append({'name': obj['name'], 'review_count': obj['review_count'], 'yelping_since': obj['yelping_since']})
+    # print(output)
+  return jsonify({'result': output})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
