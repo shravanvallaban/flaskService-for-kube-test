6967d5e661299243695240463470c36e14dc66f6
Shravan Vallaban <kuppu.shravan@gmail.com>
2021-05-08 09:37:41 -0700

    updated two A{Os

--- app.py
+++ app.py
@@ -21,6 +21,24 @@ def get_elite_users():
     # print(output)
   return jsonify({'result': output})
 
+@app.route('/deleteUsers/<int:number>', methods = ["DELETE"])
+def delete_users(number):
+    ndoc = mongo.db.myData.find({}, ('_id',), limit=number)
+    selector = {'_id': {'$in': [doc['_id'] for doc in ndoc]}}
+    result = mongo.db.myData.delete_many(selector)
+    print("The ackowledgement value is {0} and the deleted count {1}".format(result.acknowledged,  result.deleted_count))
+    return jsonify({'result': result.acknowledged})
+
+
+@app.route('/editReviewCount/<string:name>/<int:adder>', methods = ['PUT'])
+def editReviewCount(name, adder):
+  data = mongo.db.myData.find({"name":name})
+  for obj in data:
+    review_val = obj['review_count']
+    mongo.db.myData.update_one({"_id": obj["_id"]}, {"$set":{'review_count': review_val + adder}}, True)
+    # print("row is: ", obj)
+
+  return jsonify({'result': True})
 
 if __name__ == '__main__':
     app.run(debug=True)
